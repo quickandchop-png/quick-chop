c09932d05c9c932772862efeaca99ebd037ddca9
quickandchop-png <quickandchop@gmail.com>
2026-03-01 16:05:02 +0530

    Update WebSidebar.tsx

--- components/WebSidebar.tsx
+++ components/WebSidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Pressable, StyleSheet, Platform, useWindowDimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import Colors from '../../constants/colors';
+import Colors from '../constants/colors';
 
 const SIDEBAR_WIDTH = 220;
 const DESKTOP_BREAKPOINT = 768;
