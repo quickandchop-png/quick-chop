382db571757be2db0ee660868b709932c744ab88
quickandchop-png <quickandchop@gmail.com>
2026-03-03 12:40:11 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1l_PgYyod6Wlgq3ExnwTwsZbVEn4rdYE",
+  apiKey: "AIza....",
   authDomain: "quickandchop-b9b4e.firebaseapp.com",
   projectId: "quickandchop-b9b4e",
   storageBucket: "quickandchop-b9b4e.firebasestorage.app",
   messagingSenderId: "819695394751",
-  appId: "1:819695394751:web:38e7f598bfc5e87b1d4b0a",
+  appId: "1:819695394751:web:38e7f598bfc5e87b1d4b0a"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const hasFirebaseConfig = true;  // 👈 ADD THIS
